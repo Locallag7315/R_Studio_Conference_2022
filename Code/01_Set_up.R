53a1fdf4a5d0b35f72db6a14ee3a70022267f691
Locallag7315 <ocallaghanliam7315@gmail.com>
2022-07-22 15:22:52 -0400

    commit initial setup

---
+++ Code/01_Set_up.R
@@ -0,0 +1,5 @@
+install.packages(c("doParallel", "embed", "forcats",
+                   "lme4", "ranger", "remotes", "rpart", 
+                   "rpart.plot", "stacks", "tidymodels",
+                   "vetiver", "xgboost"))
+remotes::install_github("topepo/ongoal@v0.0.2")
