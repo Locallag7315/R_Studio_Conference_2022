51f3f757307235b268b2b5ffc89c3ecaec3b23d7
Locallag7315 <ocallaghanliam7315@gmail.com>
2022-07-25 16:50:48 -0400

    commiting full day 1 code

--- Code/01_Day1.R
+++ Code/01_Day1.R
@@ -94,12 +94,6 @@ linear_reg() %>%
 decision_tree() %>% 
   set_mode("regression")
 
-#tree_spec editing
-library(baguette)
-tree_Spec <- bag_tree() %>% 
-  set_mode("classification")
-
-
 #Model Specifics - Linear Regression
 lm_spec <-
   linear_reg() %>% 
@@ -121,6 +115,10 @@ augment(lm_fit, new_data = frog_test_strat)
 tree_spec <-
   decision_tree() %>% 
   set_mode("regression")
+tree_wflow <- workflow() %>%
+  add_formula(latency ~ .) %>%
+  add_model(tree_spec)
+
 
 #Fit Regression Model
 tree_fit <-
@@ -157,4 +155,232 @@ pr <- pr() %>%
 
 pr_run(pr)
 
+#######################################
+########## Model Performance ##########
+#######################################
+augment(tree_fit, new_data = frog_test_strat) %>%
+  metrics(latency, .pred)
+
+#You can get metrics by group
+augment(tree_fit, new_data = frog_test_strat) %>%
+  group_by(reflex) %>%
+  rmse(latency, .pred)
+
+#Metrics set
+frog_metrics <- metric_set(rmse, msd)
+augment(tree_fit, new_data = frog_test_strat) %>%
+  frog_metrics(latency, .pred)
+
+#Predicting on the training set
+tree_fit %>%
+  augment(frog_train_strat)
+tree_fit %>%
+  augment(frog_train_strat) %>%
+  rmse(latency, .pred)
+
+#Compare against test
+tree_fit %>%
+  augment(frog_test_strat) %>%
+  rmse(latency, .pred)
+
+#This is for demonstration purposes, don't use test until the very end
+
+#Compute MAE for train and test
+tree_fit %>%
+  augment(frog_train_strat) %>%
+  mae(latency, .pred)
+tree_fit %>%
+  augment(frog_test_strat) %>%
+  mae(latency, .pred)
+
+#R-Squared
+tree_fit %>%
+  augment(frog_train_strat) %>%
+  rsq(latency, .pred)
+tree_fit %>%
+  augment(frog_test_strat) %>%
+  rsq(latency, .pred)
+
+#R-Squared lm
+lm_fit %>%
+  augment(frog_train_strat) %>%
+  rsq(latency, .pred)
+lm_fit %>%
+  augment(frog_test_strat) %>%
+  rsq(latency, .pred)
+
+######################################
+########## Cross Validation ##########
+######################################
+#Data not used in fitting is used in predicting
+#This is done a bunch of time using re-sampling
+#Do this using the training data, don't touch the test in this
+# 10 fold is default v=10
+#you can stratify the sample similar to when the data was originally sampled
+set.seed(123)
+frog_folds <- vfold_cv(frog_train_strat, strata = "latency")
+frog_folds$splits[1:10]
+
+#Fitting the re-samples
+#Each fold will be a new model
+tree_res <- fit_resamples(tree_wflow, frog_folds)
+tree_res
+
+#Evaluate performance across the folds
+#This can be compared against the train and test metrics prior to cross-validation
+tree_res %>%
+  collect_metrics() %>% #The metrics are computed on the "test" parts of the cv analysis while the models are trained on the "training" part
+  select(.metric, mean, n)
+
+# Save the assessment set results
+ctrl_frog <- control_resamples(save_pred = TRUE) #returns a predictions column back for the individual folds
+tree_res <- fit_resamples(tree_wflow, frog_folds, control = ctrl_frog)
+
+tree_preds <- collect_predictions(tree_res)
+tree_preds
+
+#Plot all predictions across the folds
+tree_preds %>% 
+  ggplot(aes(latency, .pred, color = id)) + 
+  geom_abline(lty = 2, col = "gray", size = 1.5) +
+  geom_point(alpha = 0.5) +
+  coord_obs_pred()
+
+###################################
+########## Bootstrapping ##########
+###################################
+
+#This results in equal analysis set sizes
+#Re-sampling with replacement 
+#default is 25 times
+set.seed(3214)
+bootstraps(frog_train_strat, times = 10, strata = "latency")
+
+#Create another validation set using another re-sampling method
+set.seed(1234)
+validation_split(frog_train_strat, strata = "latency")
+
+####################################
+########## Random Forests ##########
+####################################
+
+#A bunch of decision trees
+#All the trees make individual predictions
+#An average of the decision trees is taken
+#Boot-strapping is conducted so each tree has a different set of data
+
+#Specify the model
+rf_spec <- rand_forest(trees = 1000, mode = "regression")
+rf_spec
+
+#Make the workflow
+rf_wflow <- workflow(latency ~ ., rf_spec)
+rf_wflow
+
+#Cross validation (10-fold)
+set.seed(123)
+frog_folds_rf <- vfold_cv(frog_train_strat, strata = "latency")
+
+#Re-sample and keep predicted column
+control_rf <- control_resamples(save_pred = TRUE)
+rf_res <- fit_resamples(rf_wflow, frog_folds_rf, control = control_rf)
+rf_preds <- collect_predictions(rf_res)
+
+#Compute metrics
+rf_res %>%
+  collect_metrics() %>% #The metrics are computed on the "test" parts of the cv analysis while the models are trained on the "training" part
+  select(.metric, mean, n)
+
+#Plot predictions vs. actual
+rf_preds %>% 
+  ggplot(aes(latency, .pred, color = id)) + 
+  geom_abline(lty = 2, col = "gray", size = 1.5) +
+  geom_point(alpha = 0.5) +
+  coord_obs_pred()
+
+########################################################
+########## Comparing Multiple Model Workflows ##########
+########################################################
+#Use workflow set
+#This will fit all 3, apply cv, and rank the results
+workflow_set(list(latency~.),list(tree_spec, rf_spec, lm_spec)) %>% 
+  workflow_map("fit_resamples", resamples = frog_folds) %>% 
+  rank_results()
+
+###################################
+########## The Final Fit ##########
+###################################
+#assuming we use random forest we want to fit a final time on all training data
+final_fit <- last_fit(rf_wflow, frog_split_strat)
+final_fit
+
+#metrics
+collect_metrics(final_fit) #these are computed on the test set
+
+#collect predictions
+collect_predictions(final_fit) #these are predictions on the test set
+
+#plot
+collect_predictions(final_fit) %>%
+  ggplot(aes(latency, .pred)) + 
+  geom_abline(lty = 2, col = "deeppink4", size = 1.5) +
+  geom_point(alpha = 0.5) +
+  coord_obs_pred()
+
+#Extract Workflow for final fit for rf
+extract_workflow(final_fit)
+############################
+########## Stacks ##########
+############################
+#Combine multiple model predictions into 1 prediction
+#Need to save_pred and save_workflow both true
+stack_ctrl <- control_resamples(save_pred = TRUE, save_workflow = TRUE)
+#Linear regression
+lr_res <- 
+  # define model spec
+  linear_reg() %>%
+  set_mode("regression") %>%
+  # add to workflow
+  workflow(preprocessor = latency ~ .) %>%
+  # fit to resamples
+  fit_resamples(frog_folds, control = stack_ctrl)
+#Random Forest
+rf_res <- 
+  # define model spec
+  rand_forest() %>%
+  set_mode("regression") %>%
+  # add to workflow
+  workflow(preprocessor = latency ~ .) %>%
+  # fit to resamples
+  fit_resamples(frog_folds, control = stack_ctrl)
+
+#initialize the data stack object
+library(stacks)
+frog_st <- stacks()
+
+#Add candidates to the stack
+frog_st <- frog_st %>%
+  add_candidates(lr_res) %>%
+  add_candidates(rf_res)
+
+#Combine the predictions
+frog_st_res <- frog_st %>%
+  blend_predictions()
+
+#Fit the final weights
+frog_st_res <- frog_st_res %>%
+  fit_members()
+
+#Predict on new data
+frog_test %>%
+  select(latency) %>%
+  bind_cols(
+    predict(frog_st_res, frog_test)
+  ) %>%
+  ggplot(aes(latency, .pred)) + 
+  geom_abline(lty = 2, 
+              col = "deeppink4", 
+              size = 1.5) +
+  geom_point(alpha = 0.5) +
+  coord_obs_pred()
 
